3d2475e4f9d46e601c4c10c30a616651abb9631e
Michael Ellis <67138005+miellis@users.noreply.github.com>
2024-04-27 17:22:06 +0800

    Time: 4 ms, Memory: 13.1 MB - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -2,11 +2,7 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         unordered_set<int> nums1_set(nums1.begin(), nums1.end());
-        vector<int> intersect {};
-        
-        // for (auto num1 : nums1){
-        //     nums1_set.insert(num1);
-        // }
+        vector<int> intersect;
         
         for (auto num2 : nums2){
             if(nums1_set.contains(num2)){
