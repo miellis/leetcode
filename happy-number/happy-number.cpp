8ca4965bc3a876059cd1f37d747a42b8c63735ca
Michael Ellis <67138005+miellis@users.noreply.github.com>
2024-04-28 12:25:34 +0800

    Time: 3 ms, Memory: 8.1 MB - LeetHub

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        unordered_set<int> sums;
+        int sum = 0;
+        
+        while (sums.count(sum) == 0){
+            sums.insert(sum);
+            sum = 0;
+            
+            while(n > 0){
+                int digit = n%10;
+                n = n/10;
+                sum = digit*digit + sum;
+            }
+            n = sum;
+            cout << sum << " ";
+
+        }
+        
+        if(sum == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+};
